28ee0bc2fe81627b6204cb399d556cf21077ac26
Emman-pip <115330041+Emman-pip@users.noreply.github.com>
2024-02-27 06:44:26 +0800

    Init the package.

---
+++ app/__init__.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase
+
+
+class Base(DeclarativeBase):
+    pass
+
+db = SQLAlchemy(model_class=Base)
+
+def create_app():
+    app = Flask(__name__)
+    app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///test.db'
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    
+    db.init_app(app)
+    
