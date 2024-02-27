2b0ca98c3a8bb044e711919436f0f40bb032eec7
Emman-pip <115330041+Emman-pip@users.noreply.github.com>
2024-02-27 09:03:48 +0800

    Add authentication of current user stuff and other security stuff.

--- project/__init__.py
+++ project/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase
-
+from flask_login import LoginManager
 
 class Base(DeclarativeBase):
     pass
@@ -17,6 +17,16 @@ def create_app():
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
     db.init_app(app)
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    from .models import User
+    
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
 
     from .main import main
 
--- project/auth.py
+++ project/auth.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template, url_for, redirect, request, flash
 from werkzeug.security import generate_password_hash, check_password_hash
 from .models import User
 from . import db
-
+from flask_login import login_user, logout_user
 
 auth = Blueprint("auth", __name__)
 
@@ -10,6 +10,21 @@ auth = Blueprint("auth", __name__)
 @auth.route("/login")
 def login():
     return render_template("login.html")
+
+@auth.route("/login", methods=["POST"])
+def login_post():
+    email = request.form.get('email')
+    password = request.form.get('password')
+    remember = True if request.form.get('remember') else False
+    
+    user = User.query.filter_by(email=email).first()
+
+    if not user or not check_password_hash(user.password, password):
+        flash('Invalid credentials')
+        return redirect(url_for('auth.login'))
+    
+    login_user(user, remember=remember)
+    return redirect(url_for('main.profile'))
 
 
 @auth.route("/signup")
@@ -38,4 +53,5 @@ def signup_post():
 
 @auth.route("/logout")
 def logout():
-    return "logout"
+    logout_user()
+    return redirect(url_for('auth.login'))
