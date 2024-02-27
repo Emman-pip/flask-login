8ec6533822708347495d6864f7e7fa35c4ffe3ee
Emman-pip <115330041+Emman-pip@users.noreply.github.com>
2024-02-27 08:30:09 +0800

    Fix bug.

--- project/auth.py
+++ project/auth.py
@@ -1,15 +1,15 @@
-from flask import blueprint, render_template, url_for, redirect, request, flash
+from flask import Blueprint, render_template, url_for, redirect, request, flash
 from werkzeug.security import generate_password_hash, check_password_hash
-from models import User
+from .models import User
 from . import db
 
 
-auth = blueprint("auth", __name__)
+auth = Blueprint("auth", __name__)
 
 
 @auth.route("/login")
 def login():
-    return render_template("login")
+    return render_template("login.html")
 
 
 @auth.route("/signup")
@@ -18,7 +18,7 @@ def signup():
 
 
 @auth.route("/signup", methods=["POST"])
-def signup(): 
+def signup_post(): 
     email = request.form.get('email')
     name = request.form.get('name')
     password = request.form.get('password')
@@ -27,10 +27,10 @@ def signup():
     
     if user:
         flash('You already have an account.')
-        return redirect(url_for('auth.login'))
+        return redirect(url_for('auth.signup'))
         
     
-    new_user = User(email=email, name=name, password=generate_password_hash(password, method='sha256'))
+    new_user = User(email=email, name=name, password=generate_password_hash(password, method='pbkdf2:sha1'))
     db.session.add(new_user)
     db.session.commit()
 
